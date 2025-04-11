ccbe1a6848673a8d598d567aa0e5244b0196f6c4
Mo <yanghmo98@naver.com>
2025-04-11 13:32:29 +0900

    chore: 양현모 주석 추가

--- src/components/AddTransactionButton.vue
+++ src/components/AddTransactionButton.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- 거래 추가 버튼 영역 -->
   <div class="add-button-container">
+    <!-- 버튼 클릭 시 모달 열기 -->
     <button class="add-button" @click="openTransactionModal">
       + 거래 내역 추가
     </button>
@@ -7,10 +9,12 @@
 </template>
 
 <script setup>
+// 모달 상태 관리 스토어
 import { useModalStore } from '@/stores/modalStore';
 
 const modal = useModalStore();
 
+//  거래 추가 버튼 클릭 시 동작
 const openTransactionModal = () => {
   modal.selectedDate = null; // 새로운 거래니까 날짜 지정 없음
   modal.showForm = true; // 모달 열기
@@ -18,11 +22,13 @@ const openTransactionModal = () => {
 </script>
 
 <style scoped>
+/* 버튼 위치 중앙 정렬 */
 .add-button-container {
   text-align: center;
   margin-top: 20px;
 }
 
+/* 버튼 스타일 */
 .add-button {
   background-color: #5e4b3c;
   color: white;
@@ -35,6 +41,7 @@ const openTransactionModal = () => {
   transition: background-color 0.3s;
 }
 
+/* 버튼 hover 효과 */
 .add-button:hover {
   background-color: #5e4b3c;
   color: #ffc107;
